28ddc4f70c40e773ccbcae2932ec8837819cfbe1
Gaurav1756 <gauravsenger2167@gmail.com>
2023-06-27 00:34:34 +0530

    Second

---
+++ src/components/About.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div
+      name="about"
+      className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white"
+    >
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+            About
+          </p>
+        </div>
+
+        {/* <p className="text-xl mt-20">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius,
+          deserunt illum mollitia officiis qui exercitationem perferendis neque
+          quasi a recusandae necessitatibus tempora iusto! Blanditiis error
+          iste, totam fugiat recusandae rerum laborum perferendis molestiae
+          aperiam asperiores nemo. Magni dolor maxime debitis vitae, eaque hic
+          ab mollitia voluptatibus, a nostrum eveniet laborum!
+        </p>
+
+        <br /> */}
+
+        <p className="text-xl">
+          I am Full stack web developer with good knowlwdge of HTML, CSS, Javacsript, React Js, Node JS, 
+          MongoDB and SQL. I am from Uttar Pradesh. Currently I am pursuing B.Tech from lovely Professional 
+          University.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
